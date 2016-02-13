b47d0b80137263b18084e75c552dc1a95dd4a7c2
Zygfryd Wieszok <zygfryd.wieszok@gmail.com>
2016-02-13 12:40:24 +0000

    Initial commit

---
+++ Image_classifier/MainWindow.xaml.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Image_classifier.Annotations;
+using Image_classifier.View.ViewModel;
+
+namespace Image_classifier
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private bool _barrier;
+
+        private ImageSource _currentFrame;
+        private bool _entering;
+        private bool _leaving;
+        private bool _onTrack;
+        private bool _train;
+
+        private int currentFrameIndex;
+
+        private string[] imageList;
+        private Data[] imageTags;
+        private string _imageSetPath;
+
+
+        public MainWindow()
+        {
+            InitializeMethods();
+            ImageSetPath = AppSettings.Default.ImageSetPath;
+        
+            InitializeComponent();
+        }
+
+        private void LoadImageSet()
+        {
+            currentFrameIndex = 0;
+            imageList = Directory.GetFiles(ImageSetPath, "*.png", SearchOption.AllDirectories);
+            imageList = (from im in imageList
+                select new {_Path = im, Name = Path.GetFileNameWithoutExtension(im)}).GroupBy(x => x.Name)
+                .Select(x => x.First()._Path).ToArray();
+            imageTags = new Data[imageList.Length];
+
+            if (imageList.Length == 0)
+            {
+                CurrentFrame = null;
+                MessageBox.Show("You selected empty folder. Please select folder with png images.", "Empty folder");
+                return;
+            }
+            LoadTags();
+            ClearData();
+            LoadCurrentImage();
+        }
+
+        public bool OnTrack
+        {
+            get { return _onTrack; }
+            set
+            {
+                if (value == _onTrack) return;
+                _onTrack = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool Entering
+        {
+            get { return _entering; }
+            set
+            {
+                if (value == _entering) return;
+                _entering = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool Leaving
+        {
+            get { return _leaving; }
+            set
+            {
+                if (value == _leaving) return;
+                _leaving = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool Barrier
+        {
+            get { return _barrier; }
+            set
+            {
+                if (value == _barrier) return;
+                _barrier = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool Train
+        {
+            get { return _train; }
+            set
+            {
+                if (value == _train) return;
+                _train = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ImageSource CurrentFrame
+        {
+            get { return _currentFrame; }
+            set
+            {
+                if (Equals(value, _currentFrame)) return;
+                _currentFrame = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string TaggedImagesFile { get; set; } = "tagged_images.txt";
+
+        public String ImageSetPath
+        {
+            get { return _imageSetPath; }
+            set
+            {
+                if (value == _imageSetPath) return;
+                _imageSetPath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand NextImage { get; set; }
+
+        public RelayCommand PrevImage { get; set; }
+        public RelayCommand Event1 { get; set; }
+        public RelayCommand Event2 { get; set; }
+        public RelayCommand Event3 { get; set; }
+        public RelayCommand Event4 { get; set; }
+        public RelayCommand Event5 { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void ClearData()
+        {
+            OnTrack = imageTags[currentFrameIndex].OnTrack;
+            Entering = imageTags[currentFrameIndex].Entering;
+            Leaving = imageTags[currentFrameIndex].Leaving;
+            Barrier = imageTags[currentFrameIndex].Barrier;
+            Train = imageTags[currentFrameIndex].Train;
+        }
+
+        private void SaveData()
+        {
+            imageTags[currentFrameIndex].OnTrack = OnTrack;
+            imageTags[currentFrameIndex].Entering = Entering;
+            imageTags[currentFrameIndex].Leaving = Leaving;
+            imageTags[currentFrameIndex].Barrier = Barrier;
+            imageTags[currentFrameIndex].Train = Train;
+        }
+
+        private void LoadTags()
+        {
+            if (File.Exists(TaggedImagesFile))
+            {
+                var lines = File.ReadAllLines(TaggedImagesFile);
+                for (var i = 0; i < lines.Length; i++)
+                {
+                    if (lines[i].Contains("event 1"))
+                        imageTags[i].OnTrack = true;
+                    if (lines[i].Contains(" event 2"))
+                        imageTags[i].Entering = true;
+                    if (lines[i].Contains(" event 3"))
+                        imageTags[i].Leaving = true;
+                    if (lines[i].Contains(" event 4"))
+                        imageTags[i].Barrier = true;
+                    if (lines[i].Contains(" event 5"))
+                        imageTags[i].Train = true;
+                }
+            }
+            else
+            {
+                var allImages = Directory.GetFiles(ImageSetPath, "*.png", SearchOption.AllDirectories);
+                var imDictionary = (from im in allImages
+                    select new {_Path = im, Name = Path.GetFileName(im)}).GroupBy(x => x.Name)
+                    .ToDictionary(x => x.Key, y => y.Select(z => z._Path).ToList());
+                for (var i = 0; i < imageList.Length; i++)
+                {
+                    var image = imDictionary[Path.GetFileName(imageList[i])];
+                    if (image.Any(x => x.Contains("ontrack")))
+                        imageTags[i].OnTrack = true;
+                    if (image.Any(x => x.Contains("entering")))
+                        imageTags[i].Entering = true;
+                    if (image.Any(x => x.Contains("leaving")))
+                        imageTags[i].Leaving = true;
+                    if (image.Any(x => x.Contains("barrier")))
+                        imageTags[i].Barrier = true;
+                    if (image.Any(x => x.Contains("train")))
+                        imageTags[i].Train = true;
+                }
+            }
+        }
+
+        private void InitializeMethods()
+        {
+            NextImage = new RelayCommand(obj =>
+            {
+                SaveData();
+
+                if (currentFrameIndex < imageList.Length - 1)
+                    currentFrameIndex++;
+                else
+                    MessageBox.Show("All images tagged");
+                ClearData();
+                LoadCurrentImage();
+            });
+
+            PrevImage = new RelayCommand(obj =>
+            {
+                SaveData();
+                if (currentFrameIndex > 0)
+                    currentFrameIndex--;
+                ClearData();
+                LoadCurrentImage();
+            });
+
+            Event1 = new RelayCommand(obj => OnTrack = !OnTrack);
+            Event2 = new RelayCommand(obj => Entering = !Entering);
+            Event3 = new RelayCommand(obj => Leaving = !Leaving);
+            Event4 = new RelayCommand(obj => Barrier = !Barrier);
+            Event5 = new RelayCommand(obj => Train = !Train);
+        }
+
+        private void LoadCurrentImage()
+        {
+            Title = "Image: " + (currentFrameIndex + 1) + "/" + imageList.Length + "   " +
+                    imageList[currentFrameIndex].Remove(0, ImageSetPath.Length);
+            if (imageList.Length > currentFrameIndex)
+                CurrentFrame = new BitmapImage(new Uri(imageList[currentFrameIndex]));
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            SaveDataToFile();
+            
+        }
+
+        private void SaveDataToFile()
+        {
+            using (var writer = new StreamWriter(TaggedImagesFile))
+            {
+                for (var i = 0; i < imageList.Length; i++)
+                {
+                    var tag = "";
+                    if (imageTags[i].OnTrack)
+                        tag += " event 1";
+                    if (imageTags[i].Entering)
+                        tag += " event 2";
+                    if (imageTags[i].Leaving)
+                        tag += " event 3";
+                    if (imageTags[i].Barrier)
+                        tag += " event 4";
+                    if (imageTags[i].Train)
+                        tag += " event 5";
+                    var line = Path.GetFileName(imageList[i]) + ":" + tag;
+                    writer.WriteLine(line);
+                }
+            }
+        }
+
+        private void LoadButtonClick(object sender, RoutedEventArgs e)
+        {
+            LoadTags();
+            ClearData();
+        }
+
+        private void SaveButtonClick(object sender, RoutedEventArgs e)
+        {
+            SaveDataToFile();
+        }
+
+        private struct Data
+        {
+            public bool OnTrack;
+            public bool Entering;
+            public bool Leaving;
+            public bool Barrier;
+            public bool Train;
+        }
+
+        private void ShowFolderDialog(object sender, MouseButtonEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            var result= dialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                ImageSetPath = dialog.SelectedPath;
+            }
+        }
+
+        private void LoadImageSetButtonClick(object sender, RoutedEventArgs e)
+        {
+            LoadImageSet();
+            AppSettings.Default.ImageSetPath = ImageSetPath;
+            AppSettings.Default.Save();
+        }
+    }
+}
---
+++ Results_Comparer/MainWindow.xaml.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Results_Comparer.Annotations;
+using MessageBox = System.Windows.MessageBox;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+using Path = System.IO.Path;
+
+namespace Results_Comparer
+{
+    public class Error
+    {
+        public string Name { get; set; }
+        public ImageSource ImageSource { get; set; }
+        public string FalseNegative { get; set; }
+        public string FalsePositive { get; set; }
+        public string CorrectTag { get; set; }
+        public string UserTag { get; set; }
+        public string Text { get; set; }
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _referencePath = "tagged_images.txt";
+        private string _resultsPath = "events.txt";
+        private string _results;
+
+        public string ReferencePath
+        {
+            get { return _referencePath; }
+            set
+            {
+                if (value == _referencePath) return;
+                _referencePath = value;
+                OnPropertyChanged();
+            }
+        } 
+
+        public string ResultsPath
+        {
+            get { return _resultsPath; }
+            set
+            {
+                if (value == _resultsPath) return;
+                _resultsPath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Results
+        {
+            get { return _results; }
+            set
+            {
+                if (value == _results) return;
+                _results = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool imagesPathModified = true;
+        public string ImageSetPath
+        {
+            get { return _imageSetPath; }
+            set
+            {
+                if (value == _imageSetPath) return;
+                imagesPathModified = true;
+                _imageSetPath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<Error> Errors { get; set; } = new ObservableCollection<Error>();
+        public Dictionary<string, string> ImagesDictionary;
+        private string _imageSetPath;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ImageSetPath = AppSettings.Default.ImageSetPath;
+            ReferencePath = AppSettings.Default.ReferenceFile;
+            ResultsPath = AppSettings.Default.ResultEventsFile;
+        }
+
+        private void LoadImages()
+        {
+
+            try
+            {
+                var strings = Directory.GetFiles(ImageSetPath, "*.png", SearchOption.AllDirectories);
+                ImagesDictionary = (from im in strings
+                    select new {_Path = im, Name = Path.GetFileNameWithoutExtension(im)}).GroupBy(x => x.Name)
+                    .Select(x => x.First()._Path).ToDictionary(Path.GetFileName);
+                imagesPathModified = false;
+            }
+            catch
+            {
+                MessageBox.Show("Error while loading reference images. Please specify path to reference image set.",
+                    "Error while loading images");
+                ImagesDictionary = new Dictionary<string, string>();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        private void CompareButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SaveSettings();
+                if (imagesPathModified)
+                    LoadImages();
+
+                Errors.Clear();
+                if (!File.Exists(ReferencePath))
+                {
+                    MessageBox.Show("Reference file does not exists");
+                    return;
+                }
+                var refLines = File.ReadAllLines(ReferencePath).Select(x =>
+                {
+                    var tmp = x.Split(':');
+                    return new {Name = tmp[0], Events = tmp[1].Trim()};
+                })
+                    .ToDictionary(x => x.Name,
+                        y =>
+                            y.Events.Replace("event", "")
+                                .Trim()
+                                .Replace("  ", " ")
+                                .Split(' ')
+                                .Where(x => !string.IsNullOrEmpty(x))
+                                .ToArray());
+                if (!File.Exists(ResultsPath))
+                {
+                    MessageBox.Show("Result file does not exists");
+                    return;
+                }
+                var testLines = File.ReadAllLines(ResultsPath).Select(x =>
+                {
+                    var tmp = x.Split(':');
+                    return
+                        new
+                        {
+                            Name = Path.GetFileName(tmp[0]),
+                            Events =
+                                tmp[1].Trim()
+                                    .Replace("event", "")
+                                    .Trim()
+                                    .Replace("  ", " ")
+                                    .Split(' ')
+                                    .Where(y => !string.IsNullOrEmpty(y))
+                                    .ToArray()
+                        };
+                }).ToList();
+                int correctEvents = refLines.SelectMany(x => x.Value).Count();
+                int correct = 0;
+                int falsePositives = 0;
+                int falseNegatives = 0;
+                foreach (var line in testLines)
+                {
+                    bool mistake = false;
+                    Error error = new Error() {Name = line.Name};
+                    var refLine = refLines[line.Name];
+                    foreach (var ev in line.Events)
+                    {
+                        if (refLine.Contains(ev))
+                        {
+                            correct++;
+                        }
+                        else
+                        {
+                            falsePositives++;
+                            mistake = true;
+                            error.FalsePositive += " " + ToEventName(ev) + ",";
+                        }
+                    }
+                    foreach (var ev in refLine)
+                    {
+                        if (!line.Events.Contains(ev))
+                        {
+                            falseNegatives++;
+                            mistake = true;
+                            error.FalseNegative += " " + ToEventName(ev) + ",";
+                        }
+                    }
+
+                    if (mistake)
+                    {
+                        error.CorrectTag =
+                            refLine.Select(ToEventName)
+                                .Aggregate("", (sum, eve) => sum + " " + eve + ",")?
+                                .Trim(',', ' ');
+                        error.UserTag =
+                            line.Events.Select(ToEventName)
+                                .Aggregate("", (sum, eve) => sum + " " + eve + ",")?
+                                .Trim(',', ' ');
+                        error.FalsePositive = error.FalsePositive?.Trim(',', ' ');
+                        error.FalseNegative = error.FalseNegative?.Trim(',', ' ');
+                        error.ImageSource = ImagesDictionary.ContainsKey(error.Name)
+                            ? new BitmapImage(new Uri(ImagesDictionary[error.Name]))
+                            : ((Image) Resources["NoImageSource"]).Source;
+                        error.Text = "Missing: " + error.FalseNegative + "Over classified: " + error.FalsePositive;
+                        Errors.Add(error);
+                    }
+                }
+
+                var allClassified = correctEvents + falsePositives;
+                double accuracy = correct/(double) allClassified;
+                Results =
+                    $"All correct events: {correctEvents}\nClassified events: {allClassified}\nAccuracy: {accuracy}\nFalse positive: {falsePositives} (Classified but should not be)\nFalse negative: {falseNegatives} (Not classified but should be)";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Unexpected error");
+            }
+        }
+
+        private void SaveSettings()
+        {
+            AppSettings.Default.ReferenceFile = ReferencePath;
+            AppSettings.Default.ImageSetPath = ImageSetPath;
+            AppSettings.Default.ResultEventsFile = ResultsPath;
+            AppSettings.Default.Save();
+        }
+
+        string ToEventName(string name)
+        {
+            switch (name)
+            {
+                case "1":
+                    return "OnTrack";
+                case "2":
+                    return "Entering";
+                case "3":
+                    return "Leaving";
+                case "4":
+                    return "Barrier";
+                case "5":
+                    return "Train";
+                default:
+                    return name;
+            }
+        }
+
+        private void ReferencePath_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.InitialDirectory = Path.GetDirectoryName(Path.GetFullPath(ReferencePath));
+            dialog.FileName = ReferencePath;
+            var results = dialog.ShowDialog();
+            if (results == true)
+            {
+                ReferencePath = GetPathRelativeToCurrentWorkingDirectory(dialog.FileName);
+            }
+        }
+
+        private string GetPathRelativeToCurrentWorkingDirectory(string name)
+        {
+            return GetRelativePath(name,ApplicationDirectory);
+        }
+
+        private static string ApplicationDirectory => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        string GetRelativePath(string filespec, string folder)
+        {
+            Uri pathUri = new Uri(filespec);
+            // Folders must end in a slash
+            if (!folder.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            {
+                folder += Path.DirectorySeparatorChar;
+            }
+            Uri folderUri = new Uri(folder);
+            return Uri.UnescapeDataString(folderUri.MakeRelativeUri(pathUri).ToString().Replace('/', Path.DirectorySeparatorChar));
+        }
+
+        private void EventPath_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.InitialDirectory = Path.GetDirectoryName(Path.GetFullPath(ResultsPath)); ;
+            dialog.FileName = ResultsPath;
+            var results = dialog.ShowDialog();
+            if (results == true)
+            {
+                ResultsPath = GetPathRelativeToCurrentWorkingDirectory(dialog.FileName);
+            }
+        }
+
+        private void ImageSet_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            dialog.SelectedPath = ImageSetPath;
+            var results = dialog.ShowDialog();
+            if (results == System.Windows.Forms.DialogResult.OK)
+            {
+                ImageSetPath = dialog.SelectedPath;
+            }
+        }
+    }
+}
